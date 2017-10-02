e34e7409c2d0d0b8e39dfdd51bc966a94c98cbf3
Ben Urbana <benurbana1@gmail.com>
2017-10-02 10:35:27 -0600

    Tests for has_teen?

---
+++ test.rb
@@ -0,0 +1,15 @@
+require './test_setup'
+require './Methods_quiz'
+
+describe 'Methods_quiz' do
+	
+	describe 'has_teen?' do
+		it 'should check if a number is between 12 and 20 and return true' do
+			has_teen?(2,14,20).must_equal(true)
+		end
+
+		it 'should retrun false if none of the numbers are teens' do
+			has_teen?(2,12,21).must_equal(false)
+		end
+	end
+end
