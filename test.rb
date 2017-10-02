efc10ecd640afac2f977b0cc1bcba7c45871d2f4
Ben Urbana <benurbana1@gmail.com>
2017-10-02 10:48:08 -0600

    Tests for not_string

--- test.rb
+++ test.rb
@@ -12,4 +12,15 @@ describe 'methods_quiz' do
 			has_teen?(2,12,21).must_equal(false)
 		end
 	end
+
+	describe 'not_string' do
+		it 'should add not to the front of a string' do
+			not_string('hello').must_equal('nothello')
+		end
+
+		it 'should leave the string the same if it already has not in it' do
+			not_string('nothing').must_equal('nothing')
+		end
+	end
+
 end
